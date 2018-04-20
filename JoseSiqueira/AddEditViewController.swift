24594dbbf5cca07eea0e897e4d34b89a999436a2
Otavio <persistere.5@gmail.com>
2018-04-20 18:17:59 -0300

    init

---
+++ JoseSiqueira/AddEditViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddEditViewController.swift
+//  JoseSiqueira
+//
+//  Created by Jose Otavio on 20/04/2018.
+//  Copyright © 2018 Jose Otavio. All rights reserved.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+
+    @IBOutlet var tfTitle: UITextField!
+    @IBOutlet var tfValor: UITextField!
+    @IBOutlet var ivCover: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addEditCover(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JoseSiqueira/ProdutosTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ProdutosTableViewCell.swift
+//  JoseSiqueira
+//
+//  Created by Jose Otavio on 20/04/2018.
+//  Copyright © 2018 Jose Otavio. All rights reserved.
+//
+
+import UIKit
+
+class ProdutosTableViewCell: UITableViewCell {
+
+    @IBOutlet var ivCover: UIImageView!
+    @IBOutlet var lbTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
