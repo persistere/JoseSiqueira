d5e0451754879c461fde1dd6ff009fd1d0384685
Otavio <persistere.5@gmail.com>
2018-04-26 17:27:36 -0300

    Criando Singleton para calculo

---
+++ JoseSiqueira/CalculoCompras.swift
@@ -0,0 +1,17 @@
+//
+//  CalculoCompras.swift
+//  JoseSiqueira
+//
+//  Created by Jose Otavio on 26/04/2018.
+//  Copyright © 2018 Jose Otavio. All rights reserved.
+//
+
+import Foundation
+
+class  CalculoCompras {
+    static let shared = CalculoCompras()
+    
+    private init(){
+    
+    }
+}
---
+++ JoseSiqueira/UIViewController+CalculoCompras.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+CalculoCompras.swift
+//  JoseSiqueira
+//
+//  Created by Jose Otavio on 26/04/2018.
+//  Copyright © 2018 Jose Otavio. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    var cc: CalculoCompras {
+        return CalculoCompras.shared
+    }
+}
