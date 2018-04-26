c5375696531a81bf82d8c56d93202ae3503777d4
Otavio <persistere.5@gmail.com>
2018-04-26 16:13:58 -0300

    Vinculando classe para Ajustes e Total Compra

---
+++ JoseSiqueira/AjustesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AjustesViewController.swift
+//  JoseSiqueira
+//
+//  Created by Jose Otavio on 26/04/2018.
+//  Copyright © 2018 Jose Otavio. All rights reserved.
+//
+
+import UIKit
+
+class AjustesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
